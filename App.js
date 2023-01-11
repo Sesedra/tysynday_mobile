2762a37c818d32aa0aaacf740db2a04d08503125
Sedra_Rabe <94762366+Sesedra@users.noreply.github.com>
2023-01-11 23:27:37 +0300

    Basecode

--- App.js
+++ App.js
@@ -1,12 +1,38 @@
+import * as Font from 'expo-font';
+import { AppLoading } from 'expo';
 import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import SplashScreen from './Components/SplashScreen';
+import Login from './Components/Login';
+import Home from './Components/Home';
+import { useEffect, useState } from 'react';
+import Tweets from './Components/Tweets';
 
 export default function App() {
+
+  useEffect(()=>{
+    async function loadFonts(){
+      await Font.loadAsync({
+        'Montserrat': require("./assets/font/Montserrat-Regular.ttf"),
+        'Montserrat-SemiBold': require('./assets/font/Montserrat-SemiBold.ttf'),
+        'Montserrat-Bold': require('./assets/font/Montserrat-Bold.ttf'),
+      }).then(res=>{
+        console.log("FONTS LOADED!");
+        setLoaded(true)
+      }).catch(Err=>{
+        setLoaded(true);
+        console.log(Err);
+      }); 
+    }
+
+    loadFonts();
+  },[])
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <Tweets></Tweets>
+    </SafeAreaProvider>
   );
 }
 
